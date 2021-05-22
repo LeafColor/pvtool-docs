1c3dc0c3bf265c84edeb1baf22cf428ed95c511e
likewu <lkewu@126.com>
2021-05-22 10:01:28 +0800

    init

---
+++ developers.pvtool.com/docs-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  product: "PVTool Developers",
+  pathPrefix: "",
+  productIconKey: "docs-engine",
+  contentRepo: "leafcolor/pvtool-docs",
+  contentRepoFolder: "developers.pvtool.com",
+  externalLinks: [{
+    title: "pvtool homepage",
+    url: "https://pvtool.com"
+  }],
+  search: {
+    indexName: "",
+    apiKey: "",
+    algoliaOptions: { "facetFilters": "" }
+  },
+  siteMetadata: {
+    title: "pvtool Developers",
+    description: "Browse the latest developer documentation including API reference, articles, and sample code.",
+    author: "@leafcolor",
+    url: "https://developers.pvtool.com",
+    image: "data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQEAYAAABPYyMiAAAABmJLR0T///////8JWPfcAAAACXBIWXMAAABIAAAASABGyWs+AAAAF0lEQVRIx2NgGAWjYBSMglEwCkbBSAcACBAAAeaR9cIAAAAASUVORK5CYII="
+  }
+}
